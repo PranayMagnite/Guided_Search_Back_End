f9d7b2d110ad36977dc2376832ba3c4f6f793f8e
PranayMagnite <140443649+PranayMagnite@users.noreply.github.com>
2024-11-22 11:36:36 +0000

    controllers

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const controller = require('../controllers/portalcontroller'); 
+const controller = require('../controllers/portalController'); 
 
 // JWT Secret
 const JWT_SECRET = 'aloha';
