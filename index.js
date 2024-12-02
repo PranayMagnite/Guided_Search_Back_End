acd09f7e29b020d0f9e0178ffca0e9eca313cdbf
pbhosale96 <pbhosale@dminc.com>
2024-12-02 19:01:48 +0530

    12/2

--- index.js
+++ index.js
@@ -61,14 +61,18 @@ const router = require('./routes/apiRoutes');
       var clientId = req.body.clientId;
       console.log(req.body);
       var productName = req.body.productName;
+      var productDetails = req.body.productDetails;
+      var recommendProduct = req.body.recommendedProduct;
      var META_PROMPT = `You are a Maui Jim assistant.Help users find all kind of products based on their needs and provide customer support service. Recommend products with highlighted name & sku,short information.Focus on use case, lens type, frame style, fit, and color. Ask short follow-ups if needed and suggest multiple options when unsure.response must be in markdown format`;
-     var PDP_META_PROMPT= `You are the Maui Jim Assistant for *${productName}* (details:${productDetails}). *Suggest**: Recommend alternatives from *${recommendProduct}* or complements as needed. *Purchase Info**: Offer stock, shipping, and return details. **Support**: If unsure, guide users to support. Keep replies brief and in Markdown.`;
+     var PDP_META_PROMPT_v1= `you are the Maui Jim Assistant for ${productName} product.Recommend products–Suggest alternatives or complements based on user needs.Assist with purchases – Offer stock, shipping, and return info.If unsure, direct users to support or resources.response kepp short & markdown formart`;
+     var PDP_META_PROMPT_v2= `You are the Maui Jim Assistant for *${productName}* (conside this details for provided paramerts only :${productDetails}) from this else you can provide your details. *Suggest**: Recommend alternatives from *${recommendProduct}* or complements as per user category and need. *Purchase Info**: Offer stock, shipping, and return details. **Support**: If unsure, guide users to support. Keep replies brief and in Markdown.`;
      var META_ef =`You are a mauijim website product expert providing concise advice`
 
       if (clientId) {     
         if(productName != undefined)
+        
           {
-            res.json({ metaData: PDP_META_PROMPT });
+            res.json({ metaData: PDP_META_PROMPT_v2 });
           }else{
             res.json({ metaData: META_PROMPT });
           }
